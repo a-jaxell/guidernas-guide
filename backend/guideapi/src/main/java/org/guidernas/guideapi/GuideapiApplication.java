3e023508735eaab93271a9243eb8b3ffbd3cde74
a-jaxell <97404672+a-jaxell@users.noreply.github.com>
2024-01-14 10:51:03 +0100

    First commit

---
+++ backend/guideapi/src/main/java/org/guidernas/guideapi/GuideapiApplication.java
@@ -0,0 +1,13 @@
+package org.guidernas.guideapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuideapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GuideapiApplication.class, args);
+    }
+
+}
---
+++ backend/guideapi/src/test/java/org/guidernas/guideapi/GuideapiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.guidernas.guideapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuideapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
