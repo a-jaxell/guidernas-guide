0ba42f0ce325af3cdc65b955a946761ab7217069
a-jaxell <97404672+a-jaxell@users.noreply.github.com>
2024-01-15 11:18:01 +0100

    Add service methods for finding actvities on guide or org id

--- backend/guideapi/src/main/java/org/guidernas/guideapi/activity/ActivityRepository.java
+++ backend/guideapi/src/main/java/org/guidernas/guideapi/activity/ActivityRepository.java
@@ -1,6 +1,16 @@
 package org.guidernas.guideapi.activity;
 
+import org.guidernas.guideapi.user.Guide;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface ActivityRepository extends JpaRepository<Activity, Long> {
+
+    //Selects all Activities with a specific guide as leader
+    @Query("SELECT a FROM Activity a JOIN a.leaders l WHERE l.id = :guideId")
+    List<Activity> findActivitiesByGuideId(@Param("guideId") Long guideId);
+    List<Activity> findByHostOrganizationId(Long organizationId);
 }
--- backend/guideapi/src/main/java/org/guidernas/guideapi/activity/ActivityService.java
+++ backend/guideapi/src/main/java/org/guidernas/guideapi/activity/ActivityService.java
@@ -1,8 +1,11 @@
 package org.guidernas.guideapi.activity;
 
 import org.guidernas.guideapi.exception.ActivityNotFoundException;
+import org.guidernas.guideapi.user.GuideRepository;
 import org.springframework.data.crossstore.ChangeSetPersister;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ActivityService {
@@ -15,6 +18,15 @@ public class ActivityService {
 
     public Activity getActivityById(Long id){
         return repository.findById(id).orElseThrow(ActivityNotFoundException::new);
+    }
+    public List<Activity> getAllActivities(){
+        return repository.findAll();
+    }
+    public List<Activity> getAllActivitiesByGuideId(Long guideId){
+        return repository.findActivitiesByGuideId(guideId);
+    }
+    public List<Activity> getAllActivitiesByOrganizationId(Long orgId){
+        return repository.findByHostOrganizationId(orgId);
     }
     // create new activity
     // update activity
