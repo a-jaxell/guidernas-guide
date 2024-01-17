85983418a132a5e9f0f6b73ee61b90d681425677
a-jaxell <97404672+a-jaxell@users.noreply.github.com>
2024-01-17 13:40:26 +0100

    Refactor select rendering options from enums

--- frontend/guidernas-guide/src/components/ActivityForm/ActivityForm.tsx
+++ frontend/guidernas-guide/src/components/ActivityForm/ActivityForm.tsx
@@ -12,6 +12,10 @@ const ActivityForm = ({initialData}: {initialData: Activity}) => {
   const handleSubmit = (event: React.FormEvent) => {
     event.preventDefault();
     // TODO: Use a state/store manager to set these in the state and then submit that object on submit
+    // TODO: Add validation
+    // TODO: Add error handling
+    // TODO: Add API call to submit the data
+    // TODO: Add success message
     const formData = new FormData(event.target as HTMLFormElement);
     const data = Object.fromEntries(formData.entries());
     console.log(data);
@@ -26,20 +30,16 @@ const ActivityForm = ({initialData}: {initialData: Activity}) => {
             <input name="title" maxLength={255} defaultValue={initialData.title || ""} className='input input-lg input-primary w-full max-w-xs' type="text" placeholder='Enter an title'/>
             <input name='description' maxLength={255} defaultValue={initialData.description || ""} className='textarea textarea-lg textarea-bordered w-full max-w-xs h-40' type="textarea" placeholder='Enter an Description' />
               <select name='type' defaultValue={initialData.type || "Pick an activity type"} className='select select-md select-primary w-full max-w-xs'>
-                {/* These options should be fetched as Enums */}
                 <option disabled>Pick an activity type</option>
-                <option>Skiing</option>
-                <option>Hiking</option>
-                <option>Climbing</option>
-                <option>Kayaking</option>
+                {Object.values(ActivityType).map((type) => {
+                  return <option key={type} value={type}>{type}</option>
+                })}            
               </select>
               <select name='format'defaultValue={initialData.format || "Pick an activity format"} className='select select-md select-primary w-full max-w-xs'>
-                {/* These options should be fetched as enums */}
                 <option disabled>Pick an activity format</option>
-                <option>Expedition</option>
-                <option>Daytrip</option>
-                <option>Seminar</option>
-                <option>Class</option>
+                {Object.values(ActivityFormat).map((format) => {
+                  return <option key={format} value={format}>{format}</option>
+                })}
               </select>
               {/* datepicker here https://ui.shadcn.com/docs/components/date-picker */}
               <input className='btn btn-secondary w-full max-w-xs' type="submit"/>
