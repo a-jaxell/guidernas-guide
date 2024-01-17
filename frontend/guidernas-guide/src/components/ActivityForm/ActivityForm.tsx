a8722b2d38615d6adb86cb4957f33f9f016cd369
a-jaxell <97404672+a-jaxell@users.noreply.github.com>
2024-01-17 13:31:56 +0100

    Refactor form handling

--- frontend/guidernas-guide/src/components/ActivityForm/ActivityForm.tsx
+++ frontend/guidernas-guide/src/components/ActivityForm/ActivityForm.tsx
@@ -1,52 +1,41 @@
 'use client'
-import React, { useState } from 'react'
+import { Activity, ActivityFormat, ActivityType } from '@/utils/types';
+import React, { useEffect, useState } from 'react'
 
-const ActivityForm = () => {
-  
-  const [title, setTitle] = useState('Test');
-  const [description, setDescription] = useState('A long journey');
-  const [type, setType] = useState('Hiking');
-  const [format, setFormat] = useState('Expedition');
+const ActivityForm = ({initialData}: {initialData: Activity}) => {
+  const [isEditMode, setIsEditMode] = useState(false);
 
+  useEffect(() => {
+      setIsEditMode(Boolean(initialData && initialData.id));
+  }, [initialData]);
   
   const handleSubmit = (event: React.FormEvent) => {
-    // TODO: Use a state/store manager to set these in the state and then submit that object on submit
-    const formData = new FormData(event.target);
-    // TODO : decide wether to use HTML form handling or use state with store management
     event.preventDefault();
-    console.log(formData);
-    console.log({
-      activity:{
-        title: title,
-        description: description,
-        type: type,
-        format: format
-      }
-    });
-    setTitle('');
-    setDescription('');
-    setType('Pick an activity type');
-    setFormat('Pick an activity format');
-  }
+    // TODO: Use a state/store manager to set these in the state and then submit that object on submit
+    const formData = new FormData(event.target as HTMLFormElement);
+    const data = Object.fromEntries(formData.entries());
+    console.log(data);
+
+    };
 
   return (
 
     <div className='w-4/5 h-40'>
       <h1>Create new Activity</h1>
         <form className='form-control' onSubmit={handleSubmit}>
-            <input onChange={(e)=> setTitle(e.target.value)} value={title} className='input input-lg input-primary w-full max-w-xs' type="text" placeholder='Enter an title'/>
-            <input onChange={(e)=> setDescription(e.target.value)} value={description} className='textarea textarea-lg textarea-bordered w-full max-w-xs h-40' type="textarea" placeholder='Enter an Description' />
-              <select onChange={(e)=> setType(e.target.value)} value={type} className='select select-md select-primary w-full max-w-xs'>
+            <input name="title" maxLength={255} defaultValue={initialData.title || ""} className='input input-lg input-primary w-full max-w-xs' type="text" placeholder='Enter an title'/>
+            <input name='description' maxLength={255} defaultValue={initialData.description || ""} className='textarea textarea-lg textarea-bordered w-full max-w-xs h-40' type="textarea" placeholder='Enter an Description' />
+              <select name='type' defaultValue={initialData.type || "Pick an activity type"} className='select select-md select-primary w-full max-w-xs'>
                 {/* These options should be fetched as Enums */}
-                <option disabled selected>Pick an activity type</option>
+                <option disabled>Pick an activity type</option>
                 <option>Skiing</option>
                 <option>Hiking</option>
                 <option>Climbing</option>
                 <option>Kayaking</option>
               </select>
-              <select onChange={(e)=> setFormat(e.target.value)} value={format} className='select select-md select-primary w-full max-w-xs'>
+              <select name='format'defaultValue={initialData.format || "Pick an activity format"} className='select select-md select-primary w-full max-w-xs'>
                 {/* These options should be fetched as enums */}
-                <option disabled selected>Pick an activity format</option>
+                <option disabled>Pick an activity format</option>
                 <option>Expedition</option>
                 <option>Daytrip</option>
                 <option>Seminar</option>
