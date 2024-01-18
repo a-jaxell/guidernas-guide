4ab6fb97d8d9faf9912c81bb42806d75e6294ab1
a-jaxell <97404672+a-jaxell@users.noreply.github.com>
2024-01-18 12:04:32 +0100

    Configure cors to allow frontend to access api on localhost

--- backend/guideapi/src/main/java/org/guidernas/guideapi/activity/ActivityController.java
+++ backend/guideapi/src/main/java/org/guidernas/guideapi/activity/ActivityController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/activities")
+@RequestMapping("/api/activities")
 public class ActivityController {
 
     private final ActivityService activityService;
---
+++ backend/guideapi/src/main/java/org/guidernas/guideapi/config/WebConfig.java
@@ -0,0 +1,24 @@
+package org.guidernas.guideapi.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer(){
+        return new WebMvcConfigurer() {
+            // This opens CORS and is meant as an entrypoint for the frontend or gateway
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/api/*")
+                        .allowedOrigins("http://localhost:3000")
+                        .allowedMethods("GET", "POST", "PUT","DELETE")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
