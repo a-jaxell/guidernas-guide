24b3bfba8a5762e5456de82dcbcb7bf3e851cc21
a-jaxell <97404672+a-jaxell@users.noreply.github.com>
2024-01-25 21:51:42 +0100

    Add toString capability on enums

--- backend/guideapi/src/main/java/org/guidernas/guideapi/activity/ActivityType.java
+++ backend/guideapi/src/main/java/org/guidernas/guideapi/activity/ActivityType.java
@@ -1,7 +1,26 @@
 package org.guidernas.guideapi.activity;
 
 public enum ActivityType {
-    SKIING,
-    HIKING,
-    KAYAKING
+    SKIING("Skiing"),
+    HIKING("Hiking"),
+    KAYAKING("Kayaking");
+
+    private final String value;
+
+    ActivityType(String value) {
+        this.value = value;
+    }
+
+    public String getValue(){
+        return value;
+    }
+
+    public static ActivityType fromString(String text){
+        for(ActivityType b : ActivityType.values()){
+            if(b.value.equalsIgnoreCase(text)){
+                return b;
+            }
+        }
+        throw new IllegalArgumentException("No type with " + text + " found.");
+    }
 }
